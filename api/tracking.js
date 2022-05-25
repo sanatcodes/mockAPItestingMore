44a574099900a12ddcfd9dd33cae49e599089e23
Sanat <sanat.thukral@gmail.com>
2022-05-25 23:24:57 +0100

    finally working

--- api/tracking.js
+++ api/tracking.js
@@ -24,10 +24,7 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
+    res.json(test1);
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
